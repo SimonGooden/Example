09b872658bc3e6f6a7b21096e4a1df2a971d1e47
mvcodeclub <sarah@mvcodeclub.com>
2019-04-10 19:36:12 -0700

    added tiles

--- Tile.js
+++ Tile.js
@@ -1,6 +1,31 @@
+var tilesize = 50;
+
 class Tile {
     constructor(col, row) {
         this.col = col;
         this.row = row;
+    }
+
+    draw() {
+        let x = this.col * tilesize;
+        let y = this.row * tilesize;
+
+        fill(160, 160, 180);
+        rect(x, y, tilesize, tilesize);
+
+    }
+
+    static createGrid(levelData) {
+        let numCols = levelData.size[0];
+        let numRows = levelData.size[1];
+
+        for (var r = 0; r < numRows; r++) {
+
+            for (var c = 0; c < numCols; c++) {
+
+                tiles.push(new Tile(c, r));
+
+            }
+        }
     }
 }
--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-var testTile;
+var level = {
+  size: [5, 4],
+  tiles: [
+    0, 0, 0, 0, 0,
+    0, 1, 0, 1, 0,
+    0, 1, 0, 1, 0,
+    0, 0, 0, 0, 0
+  ]
+}
+
+var tiles = [];
 
 function setup() {
-    createCanvas(500,500)
-}
-    testTile = new Tile(2, 3);
-    console.log (testTile);
+     createCanvas(500,500);
+
+     Tile.createGrid(level);
+  }
 function draw() {
     background(200);
+
+    for(var tile of tiles) {
+        tile.draw();
+    }
 }
